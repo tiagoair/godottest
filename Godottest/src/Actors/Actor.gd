999e06f522d65bb736904dc9715e9c75cc335867
tiagoair <tiagoair@gmail.com>
2023-09-18 21:35:57 -0300

    level and tilemap

--- Godottest/src/Actors/Actor.gd
+++ Godottest/src/Actors/Actor.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 class_name Actor
 
+@export var speed: = Vector2(300.0, 1000.0)
 @export var gravity: = 3000.0
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
+	velocity.y = max(velocity.y, speed.y)
 	move_and_slide()
